72a28f60177146bc9711d378fd6f10a612e5c6cb
sergioSHKLR <sergio.shklr@gmail.com>
2026-05-11 11:41:39 -0300

    Initial Eleventy setup

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const footnote = require("markdown-it-footnote");
+const toc = require("markdown-it-toc-done-right");
+
+module.exports = function(eleventyConfig) {
+
+  // Markdown plugins
+  eleventyConfig.amendLibrary("md", mdLib => {
+    mdLib.use(footnote);
+    mdLib.use(toc, {
+      containerClass: "toc",
+      listType: "ul",
+      level: [2, 3, 4]
+    });
+    return mdLib;
+  });
+
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+  };
+};
