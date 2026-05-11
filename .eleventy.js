ccc94aa08b879548a72779b5ac7d91b3872d3f6d
sergioSHKLR <sergio.shklr@gmail.com>
2026-05-11 16:23:59 -0300

    gh pages

--- .eleventy.js
+++ .eleventy.js
@@ -15,6 +15,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy(".nojekyll");
 
   return {
     dir: {
